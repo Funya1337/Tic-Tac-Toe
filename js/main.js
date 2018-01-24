49fcd779e29e1364a40abf4da6ae07cebd7cd7bb
Funya1337 <funya7654@gmail.com>
2018-01-24 21:57:17 +0300

    logirovanie

--- js/main.js
+++ js/main.js
@@ -1,12 +1,36 @@
 var flag = true;
-var array = true;
-function funcflag(elId) {
-  if (flag === true) {
-    document.getElementById(elId).className = "colorfull";
-  } else {
-    document.getElementById(elId).className = "dull";
-  }
+var foo = new Array(9);
+function clo() {
+  console.log(foo)
 }
-function funcflag1() {
+function makeMove(id) {
+  console.log('------------');
+  console.log('before makeMove()');
+  console.log('id', id);
+  console.log('flag', flag);
+  console.log('foo', foo);
+  console.log('foo[id]', foo[id]);
+
+  if (typeof foo[id] === 'undefined') {
+    var c1 = 'r' + id;
+    if (flag === true) {
+      foo[id] = 0;
+      document.getElementById(c1).className = "colorfull";
+    }  else {
+      foo[id] = 1;
+      document.getElementById(c1).className = "dull";
+    }
+  }
+  console.log('after makeMove()');
+  console.log('foo',foo);
+  console.log('foo[id]', foo[id]);
+}
+
+function changeFlag() {
+  console.log('------------');
+  console.log('before changeFlag()');
+  console.log('flag', flag);
   flag = !flag;
+  console.log('after changeFlag()');
+  console.log('flag', flag);
 }
