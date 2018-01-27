e59ae29befa625d2c9f5aa0c6fd8d8f4b5d8bbe6
Funya1337 <funya7654@gmail.com>
2018-01-27 22:46:34 +0300

    create x and 0 but not finished

--- js/main.js
+++ js/main.js
@@ -1,8 +1,31 @@
 var flag = true;
 var foo = new Array(9);
+
 function clo() {
   console.log(foo)
 }
+
+function checkBasic(value, text) {
+  if ((foo[0] === value && foo[1] === value && foo[2] === value)
+    || (foo[0] === value && foo[3] === value && foo[6] === value)
+    || (foo[1] === value && foo[4] === value && foo[7] === value)
+    || (foo[2] === value && foo[5] === value && foo[8] === value)
+    || (foo[3] === value && foo[4] === value && foo[5] === value)
+    || (foo[6] === value && foo[7] === value && foo[8] === value)
+    || (foo[0] === value && foo[4] === value && foo[8] === value)
+    || (foo[6] === value && foo[4] === value && foo[2] === value)) {
+    document.getElementById("demo").innerHTML = text;
+  }
+}
+
+function check0() {
+  checkBasic(0, "Нолики выиграли нажми F5");
+}
+
+function checkX() {
+  checkBasic(1, "Крестики выйграли нажми F5")
+}
+
 function makeMove(id) {
   console.log('------------');
   console.log('before makeMove()');
@@ -10,7 +33,6 @@ function makeMove(id) {
   console.log('flag', flag);
   console.log('foo', foo);
   console.log('foo[id]', foo[id]);
-
   if (typeof foo[id] === 'undefined') {
     var c1 = 'r' + id;
     if (flag === true) {
@@ -24,6 +46,8 @@ function makeMove(id) {
   console.log('after makeMove()');
   console.log('foo',foo);
   console.log('foo[id]', foo[id]);
+  check0();
+  checkX();
 }
 
 function changeFlag() {
